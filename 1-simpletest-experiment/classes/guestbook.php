56b30c49b904b2f361fcc34075c8fedb65654672
Frédéric Persyn <frederic.persyn@gmail.com>
2015-12-13 19:15:31 +0000

    wrote the guestbook methods with a bug in the add method for demonstration purposes

--- 1-simpletest-experiment/classes/guestbook.php
+++ 1-simpletest-experiment/classes/guestbook.php
@@ -6,19 +6,43 @@
 class Guestbook
 {
 	
+	// simulate DB connection with static entries array
+	private static $_entries = array(
+		array(
+			'name' => 'Kirk',
+			'message' => 'Hi, I\'m Kirk',
+			),
+		array(
+			'name' => 'Ted',
+			'message' => 'Hi, I\'m Ted',
+			),
+	);
+
 	public function viewAll()
 	{
-		# code...
+		// retrieve all records from the DB simulation
+		return self::$_entries;
 	}
 
 	public function add( $name, $message )
 	{
-		# code...
+		// Simulate insertion into the DB by adding a new record into $_entries
+		
+		// Correct way to do it :
+		//self::$_entries[] = array('name' => $name, 'message' => $message);
+
+		// Buggy version for demonstration purposes :
+		self::$_entries[] = array('notname' => $name, 'notmessage' => $message);
+
+		return true;
 	}
 
 	public function deleteAll()
 	{
-		# code...
+		// we just set the $_entries array to simulate
+		self::$_entries = array();
+
+		return true;
 	}
 }
 
