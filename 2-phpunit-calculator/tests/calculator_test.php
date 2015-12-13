f5a7a3009c2a626cc0c199494ba86af8e467d7c4
Frédéric Persyn <frederic.persyn@gmail.com>
2015-12-13 19:45:01 +0000

    new tutorial - wrote first phpunit test

---
+++ 2-phpunit-calculator/tests/calculator_test.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+* 
+*/
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+	
+	public function testAdd()
+	{
+		$c = New Calculator;
+		$result = $c->add(5,10);
+		$this->assertEquals(15, $result);
+	}
+}
+
+// run your tests via the CLI:
+// cd into your working directory
+// ../vendor/bin/phpunit tests/calculator_test.php
+?>
